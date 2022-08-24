c2bb7ef45f9325d73af1eb2bfdf5b67722d17723
tnrbns <69754664+tnrbns@users.noreply.github.com>
2022-08-24 20:33:08 +0800

    Update RUBINOS_MP4.py

--- MP4/RUBINOS_MP4.py
+++ MP4/RUBINOS_MP4.py
@@ -22,51 +22,51 @@ dfa_table = dfa_table.astype(int)       # Convert array of floats to integers (f
 # Converts string list to a specific value
 def strtoValue(str_in):
     # Digits 0-9
-    if(str_in=='0'): return 0;
-    elif(str_in=='1'): return 1;
-    elif(str_in=='2'): return 2;
-    elif(str_in=='3'): return 3;
-    elif(str_in=='4'): return 4;
-    elif(str_in=='5'): return 5;
-    elif(str_in=='6'): return 6;
-    elif(str_in=='7'): return 7;
-    elif(str_in=='8'): return 8;
-    elif(str_in=='9'): return 9;
+    if(str_in=='0'): return 0
+    elif(str_in=='1'): return 1
+    elif(str_in=='2'): return 2
+    elif(str_in=='3'): return 3
+    elif(str_in=='4'): return 4
+    elif(str_in=='5'): return 5
+    elif(str_in=='6'): return 6
+    elif(str_in=='7'): return 7
+    elif(str_in=='8'): return 8
+    elif(str_in=='9'): return 9
 
     # Lowercase letters (for data types)
-    elif(str_in=='a'): return 10;
-    elif(str_in=='b'): return 11;
-    elif(str_in=='c'): return 12;
-    elif(str_in=='d'): return 13;
-    elif(str_in=='e'): return 14;
-    elif(str_in=='f'): return 15;
-    elif(str_in=='h'): return 16;
-    elif(str_in=='i'): return 17;
-    elif(str_in=='l'): return 18;
-    elif(str_in=='n'): return 19;
-    elif(str_in=='o'): return 20;
-    elif(str_in=='r'): return 21;
-    elif(str_in=='t'): return 22;
-    elif(str_in=='u'): return 23;
-    elif(str_in=='v'): return 24;
+    elif(str_in=='a'): return 10
+    elif(str_in=='b'): return 11
+    elif(str_in=='c'): return 12
+    elif(str_in=='d'): return 13
+    elif(str_in=='e'): return 14
+    elif(str_in=='f'): return 15
+    elif(str_in=='h'): return 16
+    elif(str_in=='i'): return 17
+    elif(str_in=='l'): return 18
+    elif(str_in=='n'): return 19
+    elif(str_in=='o'): return 20
+    elif(str_in=='r'): return 21
+    elif(str_in=='t'): return 22
+    elif(str_in=='u'): return 23
+    elif(str_in=='v'): return 24
 
     # Other letters that aren't data types
     elif(re.search("[gjkmpqswxyz]",str_in)): return 25;
     elif(re.search("[A-Z]",str_in)): return 26;
 
     # Special characters
-    elif(str_in==' '): return 27;
-    elif(str_in=='.'): return 28;
-    elif(str_in==','): return 29;
-    elif(str_in==';'): return 30;
-    elif(str_in=='='): return 31;
-    elif(str_in=='\''): return 32;
-    elif(str_in=='('): return 33;
-    elif(str_in==')'): return 34;
-    elif(str_in=='_'): return 35;
+    elif(str_in==' '): return 27
+    elif(str_in=='.'): return 28
+    elif(str_in==','): return 29
+    elif(str_in==';'): return 30
+    elif(str_in=='='): return 31
+    elif(str_in=='\''): return 32
+    elif(str_in=='('): return 33
+    elif(str_in==')'): return 34
+    elif(str_in=='_'): return 35
 
     # Other characters not specifically mentioned
-    else: return 36;
+    else: return 36
 
 # No. of test cases
 num_testcase = int(input("No. of test cases: "))
